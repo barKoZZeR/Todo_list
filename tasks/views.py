3d677a92c6688af9104a1fb79ec095adfac2b759
barKoZZeR <2057.01@mail.ru>
2024-09-09 03:48:22 +0500

    Initial commit

---
+++ tasks/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        description = request.POST['description']
+        task = Task(title=title, description=description)
+        task.save()
+        return redirect('task_list')
+    return render(request, 'tasks/add_task.html')
+
+def toggle_task_status(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.completed = not task.completed
+    task.save()
+    return redirect('task_list')
